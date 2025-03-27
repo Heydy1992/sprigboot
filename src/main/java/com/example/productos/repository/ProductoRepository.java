8f1935ee281380fbf069e350b68766a664d3c37e
hjfz1992 <hjfz1992@gmail.com>
2025-03-26 20:18:47 -0500

    Proyecto base para estudiantes - Gestión de Productos Reactivo

---
+++ src/main/java/com/example/productos/repository/ProductoRepository.java
@@ -0,0 +1,6 @@
+// repository/ProductoRepository.java
+package com.example.productos.repository;
+import com.example.productos.model.Producto;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+public interface ProductoRepository extends ReactiveCrudRepository<Producto, String> {
+}
